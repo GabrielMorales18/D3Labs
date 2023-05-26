849eb54d33e94688b1a3a9f4ce30e8ceb2015539
GabrielMorales18 <65567283+GabrielMorales18@users.noreply.github.com>
2023-05-26 16:53:35 -0600

    Update main.js

--- 2. BindingData/js/main.js
+++ 2. BindingData/js/main.js
@@ -13,7 +13,7 @@ var rectangles = svg.selectAll("rect")
 rectangles.enter()
     .append("rect")
         .attr("x", (d,i) => { return i * 60 + 100})
-        .attr("y", (d,i) => { return i * 5 + 10 0})
+        .attr("y", (d,i) => { return i * 5 + 100})
         .attr("width", 40)
         .attr("height", (d) =>{ return d; })
         .attr("fill","gray");
